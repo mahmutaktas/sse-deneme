1cd31769dc8832cd59da4294b18288d9c17f3507
Mahmut <mahmutaktas@bitbucket.org>
2020-11-05 20:36:52 +0300

    req.txt

--- backend.py
+++ backend.py
@@ -2,4 +2,4 @@ from werkzeug.serving import run_simple
 
 import app
 
-run_simple('localhost', 8080, application=app, use_reloader=True)
+run_simple('localhost', 8085, application=app, use_reloader=True)
